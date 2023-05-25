88d851abf5d7d27d55f1d0621e7c188c2e1e6b3d
nana-boateng <n.boateng@utoronto.ca>
2023-05-25 16:04:38 -0400

    Update 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// import { SwiperModule } from 'swiper/angular';
+// import {} from ''
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +12,10 @@ import { PhotosComponent } from './pages/photos/photos.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { CommunityComponent } from './pages/community/community.component';
 import { SlidesComponent } from './components/photos/slides/slides.component';
+import { SectionOneComponent } from './components/about/section-one/section-one.component';
+import { SectionTwoComponent } from './components/about/section-two/section-two.component';
+import { SectionThreeComponent } from './components/about/section-three/section-three.component';
+import { SectionFourComponent } from './components/about/section-four/section-four.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +28,10 @@ import { SlidesComponent } from './components/photos/slides/slides.component';
     ContactComponent,
     CommunityComponent,
     SlidesComponent,
+    SectionOneComponent,
+    SectionTwoComponent,
+    SectionThreeComponent,
+    SectionFourComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  fragment = "";
+
+  constructor(private location: Location, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updateFragment();
+      }
+    });
+  }
+
+  private updateFragment() {
+    const url = this.location.path(true);
+    const fragment = url.split('#')[1] || '';
+    this.fragment = fragment;
+    console.log(this.fragment); // Output: fragment value
+  }
 }
